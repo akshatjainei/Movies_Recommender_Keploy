8e3c73854555cdfb6d86b166030d9d89ecfd7bc7
Akshat Jain <jainakshat447@gmail.com>
2023-10-04 01:52:47 +0530

    Keploy test drivers

--- src/test/java/dev/akshat/Movies_Recommender/MoviesRecommenderApplicationTests.java
+++ src/test/java/dev/akshat/Movies_Recommender/MoviesRecommenderApplicationTests.java
@@ -1,14 +1,33 @@
 //Test to be generated using Keploy
 package dev.akshat.Movies_Recommender;
 
+import io.keploy.regression.Mode;
+import io.keploy.utils.AssertKTests;
+import io.keploy.utils.HaltThread;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+
+import static dev.akshat.Movies_Recommender.MoviesRecommenderApplication.main;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class MoviesRecommenderApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void TestKeploy() throws InterruptedException {
+
+		CountDownLatch countDownLatch = HaltThread.getInstance().getCountDownLatch();
+		Mode.setTestMode();
+
+		new Thread(() -> {
+           MoviesRecommenderApplication.main(new String[]{""});
+		   countDownLatch.countDown();
+		}).start();
+
+		countDownLatch.await();
+		assertTrue(AssertKTests.result(), "Keploy Test Result");
 	}
 
 }
